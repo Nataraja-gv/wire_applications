1e0d3e9ca9851bbde15f19a8c12fd6466e22b118
nataraja <nataraja2k@gmail.com>
2024-06-25 21:41:23 +0530

    updaing

--- src/components/event-card/EventCard.jsx
+++ src/components/event-card/EventCard.jsx
@@ -2,6 +2,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { theme } from "../../theme";
+import  "./eventcard.css"
 
 
 
@@ -15,13 +16,18 @@ export default function EventCards({ Image, Title }) {
             outline: 'none',
             backgroundColor: 'transparent' ,
             // backgroundColor:"red" ,
-            height:"450px",
+            height: {
+                xs:"400px",
+                sm:"400px",
+                md:"400px",
+                lg:"450px"
+            },
             display: "flex",
             flexDirection: "column",
             alignItems: "start",
 
         }} >
-            <img src={Image} alt='' style={{ width: "100%", height: "350px",borderColor:"  #eeeeee" ,borderRadius:"5px"}} />
+            <img src={Image} alt='' className="eventimages" style={{ width: "100%",borderColor:"  #eeeeee" ,borderRadius:"5px"}} />
             <CardContent>
                 <Typography gutterBottom sx={theme.typography.eventh1}>
                     {Title}
