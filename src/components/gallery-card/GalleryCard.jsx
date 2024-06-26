2396654cae4700d416ac1d8daaa3127ed1ee81a9
nataraja <nataraja2k@gmail.com>
2024-06-26 23:21:37 +0530

    updaing

--- src/components/gallery-card/GalleryCard.jsx
+++ src/components/gallery-card/GalleryCard.jsx
@@ -1,22 +1,28 @@
-import Card from '@mui/material/Card';
+import { Card } from "@mui/material";
 
 export default function GalleryCard({ Image }) {
-    return (
-        <Card sx={{
-            width: '100%',
-            // boxShadow: 'none',  
-            // border: "none",
-            // outline: 'none',
-            // backgroundColor: 'transparent'
-            // padding: "15px"
-            height: {
-                xs:"180px",
-                sm:"230px",
-                md:"230px",
-                lg:"230px"
-            }
-        }}>
-            <img src={Image} alt='' style={{ width: "100%", height: "230px", borderRadius: "5px" }} />
-        </Card>
-    );
+  return (
+    <Card
+      sx={{
+        width: "100%",
+        // boxShadow: 'none',
+        // border: "none",
+        // outline: 'none',
+        // backgroundColor: 'transparent'
+        // padding: "15px"
+        height: {
+          xs: "180px",
+          sm: "230px",
+          md: "230px",
+          lg: "230px",
+        },
+      }}
+    >
+      <img
+        src={Image}
+        alt=""
+        style={{ width: "100%", height: "230px", borderRadius: "5px" }}
+      />
+    </Card>
+  );
 }
--- src/sections/gallery/Gallery.jsx
+++ src/sections/gallery/Gallery.jsx
@@ -1,40 +1,55 @@
 import { Grid, Stack, Typography } from "@mui/material";
 import { theme } from "../../theme";
-import GalleryCard from "../../components/gallery-card/GalleryCard"
+import GalleryCard from "../../components/gallery-card/GalleryCard";
 import { useContext } from "react";
 import { EventContext } from "../../context/tickets-context/context";
 
-
-
 const Gallery = () => {
-    const { galleryData } = useContext(EventContext)
-    return (
-        <Stack width="90%" margin=" 30px auto 0px"  >
-            <Typography variant="h2" style={{...theme.typography.title,marginBottom:"30px"}}>
-                Gallery
-            </Typography>
-            <Grid
-                container
-                spacing={2}
-                sx={{
-                    // marginTop: "5px",
-                    // backgroundColor:"green"
-
-                }}
-            >
-                {galleryData.map((menu, index) => (
-                    <Grid item xs={6} sm={6} md={3} key={index}>
-                        <GalleryCard
-                            Image={menu.img}
-
-                        />
-                    </Grid>
-                ))}
-            </Grid>
-
-
-        </Stack>
-    );
-}
+  const { galleryData } = useContext(EventContext);
+  return (
+    <Stack
+      width="90%"
+      sx={{
+        margin: {
+          xs: "50px auto 0px",
+          sm: "10px auto 0px",
+          md: "50px auto 0px",
+          lg: "50px auto 20px",
+        },
+      }}
+    >
+      <Typography
+        variant="title"
+        sx={{
+          fontSize: {
+            xs: "25px",
+            sm: "30px",
+            md: "40px",
+            lg: "40px",
+          },
+          marginBottom: "30px",
+        }}
+      >
+        Gallery
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        sx={
+          {
+            // marginTop: "5px",
+            // backgroundColor:"green"
+          }
+        }
+      >
+        {galleryData.map((menu, index) => (
+          <Grid item xs={6} sm={6} md={3} key={index}>
+            <GalleryCard Image={menu.img} />
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
+  );
+};
 
 export default Gallery;
